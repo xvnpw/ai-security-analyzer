0f71c3523a0eac04f74b75fd7c4df296b64bc01d
xvnpw <17719543+xvnpw@users.noreply.github.com>
2024-12-14 12:10:49 +0100

    fix build sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 npm install
-poetry config virtualenvs.create false
-poetry install --no-root --no-interaction --no-ansi
+poetry install --no-interaction --no-ansi
 scripts/fix_mermaid_dompurify.sh
