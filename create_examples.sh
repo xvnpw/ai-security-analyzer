a5894b56d3eba05427d46d4d0c8bab6ddedf113f
xvnpw <17719543+xvnpw@users.noreply.github.com>
2025-02-01 15:25:34 +0100

    Readme

--- create_examples.sh
+++ create_examples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AGENT_PROMPT_TYPES="sec-design threat-modeling attack-surface attack-tree mitigations"
+AGENT_PROMPT_TYPES="sec-design attack-tree"
 
 declare -A models
 # models["o1"]="openai"
@@ -12,7 +12,7 @@ declare -A temperatures
 temperatures["o1"]="1"
 temperatures["o3-mini"]="1"
 temperatures["gemini-2.0-flash-thinking-exp"]="0"
-temperatures["deepseek/deepseek-r1"]="0"
+temperatures["deepseek/deepseek-r1"]="0.2"
 
 # printf "# Examples\n\n" > examples/README.md
 printf "## dir mode\n\n" >> examples/README.md
@@ -39,53 +39,53 @@ for agent_prompt_type in $AGENT_PROMPT_TYPES; do
     done
 done
 
-printf "\n## file mode\n\n" >> examples/README.md
+# printf "\n## file mode\n\n" >> examples/README.md
 
-printf "| Project Name | Project Type | Model | Documentation |\n" >> examples/README.md
-printf "| --- | --- | --- | --- |\n" >> examples/README.md
+# printf "| Project Name | Project Type | Model | Documentation |\n" >> examples/README.md
+# printf "| --- | --- | --- | --- |\n" >> examples/README.md
 
-for agent_prompt_type in $AGENT_PROMPT_TYPES; do
-    # Iterate over the keys of the models array
-    for agent_model in "${!models[@]}"; do
-        agent_provider="${models[$agent_model]}"
-        safe_agent_model=$(echo $agent_model | tr '[:upper:]' '[:lower:]' | tr -cd '[:alnum:]_.-')
+# for agent_prompt_type in $AGENT_PROMPT_TYPES; do
+#     # Iterate over the keys of the models array
+#     for agent_model in "${!models[@]}"; do
+#         agent_provider="${models[$agent_model]}"
+#         safe_agent_model=$(echo $agent_model | tr '[:upper:]' '[:lower:]' | tr -cd '[:alnum:]_.-')
 
-        echo "Generating example for $agent_prompt_type with $agent_model"
+#         echo "Generating example for $agent_prompt_type with $agent_model"
 
-        ARGS="file -t tests/EXAMPLE_ARCHITECTURE.md -v -o examples/file-${agent_prompt_type}-ai-nutrition-pro-${safe_agent_model}.md --agent-model $agent_model --agent-temperature ${temperatures[$agent_model]} --agent-prompt-type $agent_prompt_type --agent-provider $agent_provider"
+#         ARGS="file -t tests/EXAMPLE_ARCHITECTURE.md -v -o examples/file-${agent_prompt_type}-ai-nutrition-pro-${safe_agent_model}.md --agent-model $agent_model --agent-temperature ${temperatures[$agent_model]} --agent-prompt-type $agent_prompt_type --agent-provider $agent_provider"
 
-        CMD="python ai_security_analyzer/app.py $ARGS"
-        echo "Running: $CMD"
+#         CMD="python ai_security_analyzer/app.py $ARGS"
+#         echo "Running: $CMD"
 
-        python ai_security_analyzer/app.py $ARGS
+#         python ai_security_analyzer/app.py $ARGS
 
-        printf "| [AI Nutrition-Pro](../tests/EXAMPLE_ARCHITECTURE.md)<br/><details><summary>commands...</summary>\`\`\`bash %s\`\`\`</details> | python | $agent_model | [${agent_prompt_type}](file-${agent_prompt_type}-ai-nutrition-pro-${safe_agent_model}.md) |\n" "$CMD" >> examples/README.md
-    done
-done
+#         printf "| [AI Nutrition-Pro](../tests/EXAMPLE_ARCHITECTURE.md)<br/><details><summary>commands...</summary>\`\`\`bash %s\`\`\`</details> | python | $agent_model | [${agent_prompt_type}](file-${agent_prompt_type}-ai-nutrition-pro-${safe_agent_model}.md) |\n" "$CMD" >> examples/README.md
+#     done
+# done
 
-printf "\n## github mode\n\n" >> examples/README.md
+# printf "\n## github mode\n\n" >> examples/README.md
 
-printf "| Project Name | Project Type | Model | Documentation |\n" >> examples/README.md
-printf "| --- | --- | --- | --- |\n" >> examples/README.md
+# printf "| Project Name | Project Type | Model | Documentation |\n" >> examples/README.md
+# printf "| --- | --- | --- | --- |\n" >> examples/README.md
 
-for agent_prompt_type in $AGENT_PROMPT_TYPES; do
-    # Iterate over the keys of the models array
-    for agent_model in "${!models[@]}"; do
-        agent_provider="${models[$agent_model]}"
-        safe_agent_model=$(echo $agent_model | tr '[:upper:]' '[:lower:]' | tr -cd '[:alnum:]_.-')
+# for agent_prompt_type in $AGENT_PROMPT_TYPES; do
+#     # Iterate over the keys of the models array
+#     for agent_model in "${!models[@]}"; do
+#         agent_provider="${models[$agent_model]}"
+#         safe_agent_model=$(echo $agent_model | tr '[:upper:]' '[:lower:]' | tr -cd '[:alnum:]_.-')
 
-        echo "Generating example for $agent_prompt_type with $agent_model"
+#         echo "Generating example for $agent_prompt_type with $agent_model"
 
-        ARGS="github -t https://github.com/pallets/flask -v -o examples/github-${agent_prompt_type}-flask-${safe_agent_model}.md --agent-model $agent_model --agent-temperature ${temperatures[$agent_model]} --agent-prompt-type $agent_prompt_type --agent-provider $agent_provider"
+#         ARGS="github -t https://github.com/pallets/flask -v -o examples/github-${agent_prompt_type}-flask-${safe_agent_model}.md --agent-model $agent_model --agent-temperature ${temperatures[$agent_model]} --agent-prompt-type $agent_prompt_type --agent-provider $agent_provider"
 
-        CMD="python ai_security_analyzer/app.py $ARGS"
-        echo "Running: $CMD"
+#         CMD="python ai_security_analyzer/app.py $ARGS"
+#         echo "Running: $CMD"
 
-        python ai_security_analyzer/app.py $ARGS
+#         python ai_security_analyzer/app.py $ARGS
 
-        printf "| [flask](https://github.com/pallets/flask)<br/><details><summary>commands...</summary>\`\`\`bash %s\`\`\`</details> | python | $agent_model | [${agent_prompt_type}](github-${agent_prompt_type}-flask-${safe_agent_model}.md) |\n" "$CMD" >> examples/README.md
-    done
-done
+#         printf "| [flask](https://github.com/pallets/flask)<br/><details><summary>commands...</summary>\`\`\`bash %s\`\`\`</details> | python | $agent_model | [${agent_prompt_type}](github-${agent_prompt_type}-flask-${safe_agent_model}.md) |\n" "$CMD" >> examples/README.md
+#     done
+# done
 
 # printf "\n## github mode - deep analysis\n\n" >> examples/README.md
 
@@ -120,26 +120,26 @@ done
 ########################
 
 # printf "# form3tech-oss Examples\n\n" > examples/form3tech-oss/README.md
-printf "## dir mode\n\n" >> examples/form3tech-oss/README.md
+# printf "## dir mode\n\n" >> examples/form3tech-oss/README.md
 
-printf "| Project Name | Project Type | Model | Documentation |\n" >> examples/form3tech-oss/README.md
-printf "| --- | --- | --- | --- |\n" >> examples/form3tech-oss/README.md
+# printf "| Project Name | Project Type | Model | Documentation |\n" >> examples/form3tech-oss/README.md
+# printf "| --- | --- | --- | --- |\n" >> examples/form3tech-oss/README.md
 
-for agent_prompt_type in $AGENT_PROMPT_TYPES; do
-    # Iterate over the keys of the models array
-    for agent_model in "${!models[@]}"; do
-        agent_provider="${models[$agent_model]}"
-        safe_agent_model=$(echo $agent_model | tr '[:upper:]' '[:lower:]' | tr -cd '[:alnum:]_.-')
+# for agent_prompt_type in $AGENT_PROMPT_TYPES; do
+#     # Iterate over the keys of the models array
+#     for agent_model in "${!models[@]}"; do
+#         agent_provider="${models[$agent_model]}"
+#         safe_agent_model=$(echo $agent_model | tr '[:upper:]' '[:lower:]' | tr -cd '[:alnum:]_.-')
 
-        echo "Generating example for $agent_prompt_type with $agent_model"
+#         echo "Generating example for $agent_prompt_type with $agent_model"
 
-        ARGS="dir -t ../terraform-provider-chronicle/ -v -o examples/form3tech-oss/dir-${agent_prompt_type}-terraform-provider-chronicle-${safe_agent_model}.md --agent-model $agent_model --agent-temperature ${temperatures[$agent_model]} --agent-prompt-type $agent_prompt_type --agent-provider $agent_provider -p go --include **/*.tf,**/*.tmpl,**/GNUmakefile"
+#         ARGS="dir -t ../terraform-provider-chronicle/ -v -o examples/form3tech-oss/dir-${agent_prompt_type}-terraform-provider-chronicle-${safe_agent_model}.md --agent-model $agent_model --agent-temperature ${temperatures[$agent_model]} --agent-prompt-type $agent_prompt_type --agent-provider $agent_provider -p go --include **/*.tf,**/*.tmpl,**/GNUmakefile"
 
-        CMD="python ai_security_analyzer/app.py $ARGS"
-        echo "Running: $CMD"
+#         CMD="python ai_security_analyzer/app.py $ARGS"
+#         echo "Running: $CMD"
 
-        python ai_security_analyzer/app.py $ARGS
+#         python ai_security_analyzer/app.py $ARGS
 
-        printf "| [terraform-provider-chronicle](https://github.com/form3tech-oss/terraform-provider-chronicle)<br/><details><summary>commands...</summary>\`\`\`bash %s\`\`\`</details> | python | $agent_model | [${agent_prompt_type}](dir-${agent_prompt_type}-terraform-provider-chronicle-${safe_agent_model}.md) |\n" "$CMD" >> examples/form3tech-oss/README.md
-    done
-done
+#         printf "| [terraform-provider-chronicle](https://github.com/form3tech-oss/terraform-provider-chronicle)<br/><details><summary>commands...</summary>\`\`\`bash %s\`\`\`</details> | python | $agent_model | [${agent_prompt_type}](dir-${agent_prompt_type}-terraform-provider-chronicle-${safe_agent_model}.md) |\n" "$CMD" >> examples/form3tech-oss/README.md
+#     done
+# done
